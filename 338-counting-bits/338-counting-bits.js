d56bb186db34eec9a7bbfaf185c8d6932f2de46e
Jaeuk Lim <applewodnr@naver.com>
2022-05-12 21:54:14 +0900

    Time: 155 ms (24.53%), Space: 49.3 MB (50.87%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -3,32 +3,13 @@
  * @return {number[]}
  */
 var countBits = function(n) {
-    const dic = new Map([[0,0]]);
+    const dp = [0];
     
-    const countBit = (n) => {
-        if(dic.has(n)) return dic.get(n);
-            
-        let cnt = 0
-        let tmp = n;
-        
-        while(tmp > 0){
-            tmp = tmp & tmp-1
-            
-            if(dic.has(tmp)) {
-                dic.set(n, dic.get(tmp) + cnt + 1);
-                return dic.get(n);
-            }
-        
-            cnt += 1
-        }
-        
-        dic.set(n, cnt);
-        return dic.get(n);
+    for(let i=1; i<=n; i++){
+        const exponent = Math.floor(Math.log2(i));
+        const mod = 2 ** exponent;
+        dp.push(dp[i % mod] + 1);
     }
     
-    for(let i = 1; i <= n; i++){
-        countBit(i);
-    }
-    
-    return Array.from(dic.values());
+    return dp;
 };
