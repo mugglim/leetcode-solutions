8c2d3e9c443e436ba83b8b71f2f657f1298be77b
Jaeuk Lim <applewodnr@naver.com>
2022-05-21 13:57:17 +0900

    Time: 561 ms (5.01%), Space: 51.5 MB (5.14%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.js
+++ 283-move-zeroes/283-move-zeroes.js
@@ -5,16 +5,20 @@
 var moveZeroes = function(nums) {    
     const n = nums.length;
     let hi = 0;
-    
-    const swap = (lo,hi) => ([nums[lo], nums[hi]] = [nums[hi], nums[lo]]);
 
     for(let lo=0; lo < n ; lo += 1){
         if(nums[hi] !== 0) hi = lo;
         
         while(hi < n && nums[hi] == 0) hi += 1;        
-        if(hi < n) swap(lo,hi);
+        
+        if(hi < n) {
+            [nums[lo], nums[hi]] = [nums[hi], nums[lo]];
+        }
         
         console.log(lo,hi);
     }
     
 };
+
+
+
