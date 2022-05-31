a7875322e9a3bef3d6756dcce2417eb2a7a4148b
Jaeuk Lim <applewodnr@naver.com>
2022-05-31 15:14:55 +0900

    Time: 973 ms (22.40%), Space: 119.4 MB (30.25%) - LeetHub

--- 204-count-primes/204-count-primes.js
+++ 204-count-primes/204-count-primes.js
@@ -6,6 +6,7 @@ var countPrimes = function(n) {
     
     const isPrime = (x) => {
         const sqrtX = Math.floor(Math.sqrt(x));
+        console.log(sqrtX);
         
         for(let i = 2; i <= sqrtX; i+= 1){
             if(x % i === 0) return false;
@@ -13,11 +14,11 @@ var countPrimes = function(n) {
         return true;
     }
         
-    // // (1) O(n * n)
+    // // (1) O(nloglogn)
     const prime = new Array(n).fill(true);
-    
+        
     [prime[0], prime[1]] = [false, false];
-    
+        
     for(let i = 2; i < n; i += 1){
         if(prime[i]){
             for(let j = i * 2; j < n; j += i){
