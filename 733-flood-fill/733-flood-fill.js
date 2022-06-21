6a562b6ed1a1c33c80c1c1f55c138bac9e7d2533
Jaeuk Lim <applewodnr@naver.com>
2022-06-21 21:27:06 +0900

    Time: 126 ms (22.93%), Space: 45.2 MB (7.50%) - LeetHub

--- 733-flood-fill/733-flood-fill.js
+++ 733-flood-fill/733-flood-fill.js
@@ -9,27 +9,23 @@ var floodFill = function(image, sr, sc, color) {
     const [n,m] = [image.length, image[0].length];
     const visited = Array.from({length : n}, () => Array.from(m).fill(false));
     const dirs = [[-1,0], [1,0], [0,1], [0,-1]];
-    const queue = [[sr,sc, image[sr][sc]]];
-
     const isRange = (y,x) => 0 <= y && y < n & 0 <= x && x < m;
+    const prevColor = image[sr][sc];
     
-    visited[sr][sc] = true;
-    image[sr][sc] = color;
-    
-    
-    while(queue.length > 0) {
-        const [y,x, prevColor] = queue.shift();
+    const dfs = (y,x) => {
+        visited[y][x] = true;
+        image[y][x] = color;
         
         for(const [dy,dx] of dirs){
             const [ny,nx] = [y+dy, x+dx];
-            
-            if(!isRange(ny,nx) || visited[ny][nx] || prevColor !== image[ny][nx]) continue
-            
-            visited[ny][nx] = true;
-            image[ny][nx] = color;
-            queue.push([ny,nx, prevColor]);
+            if(isRange(ny,nx) && !visited[ny][nx] && prevColor === image[ny][nx]){
+                dfs(ny,nx);
+            }
         }
     }
     
+    dfs(sr,sc);
     return image;
 };
+
+
