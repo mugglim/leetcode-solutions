4925fb1f9fd7748aa0628973ae881d7f641ca8f6
Jaeuk Lim <applewodnr@naver.com>
2022-06-23 15:50:25 +0900

    Time: 201 ms (70.40%), Space: 61.7 MB (100.00%) - LeetHub

--- 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
+++ 1161-maximum-level-sum-of-a-binary-tree/1161-maximum-level-sum-of-a-binary-tree.js
@@ -1,30 +1,29 @@
-
 var maxLevelSum = function(root) {
-    const queue = [[root, 1]];
-    const levelSumMap = new Map();
+    const queue = [[[root],1]];
     const childNodeProps = ['left', 'right'];
+    let maxLevel = 1;
+    let maxSum = root.val;
     
-    const getMaxSumLevel = () => {
-        const cmp = (prevNode, currNode) => {
-            return prevNode[1] < currNode[1] ? currNode : prevNode;
+    while(true){
+        const [rootNodes, level] = queue.shift();
+        const childNodes = [];
+        let childNodeSum = 0;
+        
+        for(const rootNode of rootNodes){
+            for(const prop of childNodeProps){
+                const childNode = rootNode[prop];
+                if(childNode !== null) {
+                    childNodes.push(childNode);
+                    childNodeSum += childNode.val;
+                }
+            }
         }
         
-        return Array
-                .from(levelSumMap)
-                .reduce(cmp)[0]
+        if(childNodes.length === 0) break;
+
+        if(maxSum < childNodeSum) [maxLevel, maxSum] = [level+1, childNodeSum]
+        queue.push([childNodes, level+1])
     }
     
-    while(queue.length > 0){
-        const [node, level] = queue.shift();
-        if(!levelSumMap.has(level)) levelSumMap.set(level, 0);
-        
-        levelSumMap.set(level, levelSumMap.get(level) + node.val);
-        
-        for(const prop of childNodeProps){
-            const childNode = node[prop];
-            if(childNode !== null) queue.push([childNode, level+1]);
-        }
-    }
-    
-    return getMaxSumLevel();
+    return maxLevel;
 };
