9ed06bd9cf02d0ca8be8b969552f3aab5a18e59b
Jaeuk Lim <applewodnr@naver.com>
2022-07-12 16:48:51 +0900

    Time: 213 ms (5.10%), Space: 56 MB (5.15%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -7,20 +7,20 @@ function *range(s=0, e, step=1){
     }
 }
 
-/**
+/** 
  * @param {number[]} nums
  * @return {number}
  */
-var maxSubArray = function(nums) {
+var maxSubArray = function(nums) { 
     // @Kadane's algorithm
     const n = nums.length;
-    const sums = new Array(n).fill(0);
-    
-    sums[0] = nums[0];
+    let sum = nums[0];
+    let maxSum = nums[0];
     
     for(const i of range(1,n)){
-        sums[i] = Math.max(nums[i], nums[i] + sums[i-1]);
+        sum = Math.max(nums[i], sum + nums[i]);
+        maxSum = Math.max(sum, maxSum);
     }
     
-    return Math.max(...sums);
+    return maxSum;
 };
