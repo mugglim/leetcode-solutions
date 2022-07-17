eb8e4418c6589257be1d159096a0cd9be3df1dff
Jaeuk Lim <applewodnr@naver.com>
2022-07-17 18:11:07 +0900

    Time: 275 ms (39.90%), Space: 59.7 MB (51.17%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
@@ -12,24 +12,18 @@
  * @return {number[]}
  */
 var getAllElements = function(root1, root2) {
-    const MAX = 10 ** 5;
-    const counts = new Array(MAX * 2 + 2).fill(0);
     const nums = [];
     
     const search = (node) => {
         if(!node) return;
         search(node.left);
-        counts[node.val + MAX] += 1;
+        nums.push(node.val)
         search(node.right);
     }
     
     search(root1);
     search(root2);
 
-    counts.forEach((count, idx) => {
-        if(count === 0) return;
-        nums.push(...new Array(count).fill(idx - MAX));
-    })
     
-    return nums;
+    return nums.sort((a,b) => a - b);
 };
